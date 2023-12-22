adda10986e84f317b9e0b3eee21436b3e8dc0d27
beckpalmx77 <beckpalmx@gmail.com>
2023-12-22 17:31:19 +0700

    Update Header.php

--- includes/Header.php
+++ includes/Header.php
@@ -13,7 +13,7 @@
 
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/myadmin.css" rel="stylesheet">
+    <link href="css/myadmin.css?v=1001" rel="stylesheet">
 
     <link rel="stylesheet" href="vendor/alertify/css/alertify.core.css"/>
     <link rel="stylesheet" href="vendor/alertify/css/alertify.default.css"/>
